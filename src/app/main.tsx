f7725a93072aa2141df977d7fe2ade4d13958973
saiani1 <saiani@naver.com>
2025-06-01 12:58:43 +0900

    recoil세팅

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,11 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
+import App from './App.tsx';
 
-import "./index.css";
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </StrictMode>
 );
