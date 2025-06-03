113e0a93d39460b07fd24f0b19c9b2834ab4e1e6
saiani1 <saiani@naver.com>
2025-06-03 16:13:12 +0900

    HomeScreen컴포넌트 UI완성

--- src/pages/Home/ui/HomeScreen.tsx
+++ src/pages/Home/ui/HomeScreen.tsx
@@ -1,7 +1,34 @@
+import { IoIosArrowBack, IoIosMenu } from 'react-icons/io';
+import { GrDocumentText } from 'react-icons/gr';
+
+import { ToDoList } from '@features/ToDoList';
+import { CommonButton } from '@shared/ui';
+import { DUMMY_TODO } from '@entities/ToDoList';
+
 const HomeScreen = () => {
   return (
-    <div>
-      <p>d</p>
+    <div className="relative flex flex-col w-full h-screen bg-blue-400">
+      <div className="fixed flex justify-between px-[20px] pt-[30px] w-full z-200">
+        <CommonButton className="flex justify-center items-center w-[40px] h-[40px] rounded-full bg-blue-400/80">
+          <IoIosArrowBack size="30" fill="#fff" className="-ml-[3px]" />
+        </CommonButton>
+        <CommonButton className="flex justify-center items-center w-[40px] h-[40px] rounded-full bg-blue-400/80">
+          <IoIosMenu size="30" fill="#fff" />
+        </CommonButton>
+      </div>
+      <div className="fixed flex flex-col gap-y-3 px-[55px] pt-[110px] z-10">
+        <div className="flex justify-center items-center w-[46px] h-[46px] rounded-full bg-white">
+          <GrDocumentText size="22" stroke="#60a5fa" />
+        </div>
+        <div className="flex flex-col">
+          <h1 className="font-semibold text-[35px] text-white">All</h1>
+          <span className="-mt-[10px] text-white">23 Tasks</span>
+        </div>
+      </div>
+      <ul className="absolute bottom-0 w-full h-[60%] px-[45px] py-[30px] bg-white rounded-t-2xl z-100">
+        <ToDoList title="진행중" data={DUMMY_TODO} />
+        <ToDoList title="완료" data={DUMMY_TODO} />
+      </ul>
     </div>
   );
 };
