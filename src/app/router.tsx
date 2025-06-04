1c776e4fbea4d8a72767602ca3af945a83b1f43e
saiani1 <saiani@naver.com>
2025-06-04 11:34:00 +0900

    create라우터 추가

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,10 +1,20 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import HomeScreen from '@pages/Home/ui/HomeScreen';
+import CreateToDoScreen from '@pages/ToDo/ui/CreateToDoScreen';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomeScreen />,
+    children: [
+      {
+        path: '/',
+        element: <HomeScreen />,
+      },
+      {
+        path: 'create',
+        element: <CreateToDoScreen />,
+      },
+    ],
   },
 ]);
