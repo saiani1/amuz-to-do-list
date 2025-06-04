37ac9c98176468aad3aac1d5348cae89711ede39
saiani1 <saiani@naver.com>
2025-06-04 11:50:44 +0900

    CommonInput컴포넌트 forwardRef추가

--- src/shared/ui/CommonInput.tsx
+++ src/shared/ui/CommonInput.tsx
@@ -1,13 +1,19 @@
+import { forwardRef } from 'react';
+
 type CommonInputType = React.ComponentProps<'input'> & {
   className?: string;
 };
 
-export const CommonInput = ({ className, ...rest }: CommonInputType) => {
-  return (
-    <input
-      {...rest}
-      type={rest.type ?? 'text'}
-      className={`${className ?? ''} focus:outline-none`}
-    />
-  );
-};
+export const CommonInput = forwardRef<HTMLInputElement, CommonInputType>(
+  ({ className, ...rest }, ref) => {
+    return (
+      <input
+        ref={ref}
+        {...rest}
+        type={rest.type ?? 'text'}
+        className={`${className ?? ''} focus:outline-none`}
+      />
+    );
+  }
+);
+CommonInput.displayName = 'CommonInput';
