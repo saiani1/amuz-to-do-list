dae5f8e587450f9b8bc8c2b8c5496516e6418f83
saiani1 <saiani@naver.com>
2025-06-04 10:35:50 +0900

    CategorySelectBox컴포넌트 추가

---
+++ src/features/Category/ui/CategorySelectBox.tsx
@@ -0,0 +1,60 @@
+import { useRef, useState } from 'react';
+
+import type { CategoryType } from '@entities/Category';
+import { useOnClickOutside } from '@shared/hook';
+import { CommonButton } from '@shared/ui';
+
+type CategorySelectBoxType = {
+  data: CategoryType[];
+  clickedOption: string;
+  setClickedOption: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export const CategorySelectBox = ({
+  data,
+  clickedOption,
+  setClickedOption,
+}: CategorySelectBoxType) => {
+  const ref = useRef(null);
+  const [clickedBox, setClickedBox] = useState(false);
+
+  const handleClickOption = (e: React.MouseEvent<HTMLUListElement>) => {
+    const value = (e.target as HTMLButtonElement).name;
+    if (value) setClickedOption(value);
+    setClickedBox(false);
+  };
+
+  const handleClickBox = () => setClickedBox((prev) => !prev);
+  useOnClickOutside(ref, () => setClickedBox(false));
+
+  return (
+    <div
+      ref={ref}
+      className="relative text-gray-400 focus-within:text-gray-400"
+    >
+      <CommonButton
+        className={`px-3 py-1 rounded-2xl ${clickedOption === '카테고리' ? 'text-gray-400' : 'font-medium text-gray-600'}`}
+        onClick={handleClickBox}
+      >
+        {clickedOption}
+      </CommonButton>
+      {clickedBox && (
+        <ul
+          className="absolute left-1/2 top-[38px] -translate-x-1/2 w-30 text-center divide-y border rounded-md"
+          onClick={handleClickOption}
+        >
+          {data.map((item, i) => (
+            <li key={`category-${i}`} data-option={item}>
+              <CommonButton
+                name={item.name}
+                className="px-[4px] py-[3px] w-full"
+              >
+                {item.name}
+              </CommonButton>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
