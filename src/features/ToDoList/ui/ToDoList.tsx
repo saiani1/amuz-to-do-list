f9ea00eca821b863c30f16dabdd1037f89b83ab2
saiani1 <saiani@naver.com>
2025-06-06 20:43:28 +0900

    todo기능 CRUD api연결

--- src/features/ToDoList/ui/ToDoList.tsx
+++ src/features/ToDoList/ui/ToDoList.tsx
@@ -1,18 +1,47 @@
+import { useEffect, useState } from 'react';
+
 import type { ToDoType } from '@entities/ToDoList';
 import { ToDo } from './ToDo';
+import { supabase } from '@shared/api';
 
 type ToDoListType = {
-  title: string;
-  data: ToDoType[];
+  categoryId: string;
+  setTaskNum: React.Dispatch<React.SetStateAction<number>>;
 };
 
-export const ToDoList = ({ title, data }: ToDoListType) => {
+export const ToDoList = ({ categoryId, setTaskNum }: ToDoListType) => {
+  const [isChanged, setIsChanged] = useState(true);
+  const [todoData, setTodoData] = useState<ToDoType[]>();
+
+  useEffect(() => {
+    (async () => {
+      if (isChanged) {
+        const { data: todos, error } = await supabase
+          .from('todos')
+          .select('*')
+
+          .eq('category_id', categoryId)
+          .order('is_checked', { ascending: true })
+          .order('is_important', { ascending: false });
+        if (error) console.log(error);
+        if (todos) {
+          setTodoData(todos);
+          setIsChanged(false);
+          setTaskNum(todos.length);
+        }
+      }
+    })();
+  }, [isChanged]);
+
   return (
     <li className="bg-white">
-      <h3 className="mb-5 text-[14px] text-[#AAA]">{title}</h3>
       <ul className="flex flex-col gap-y-3 pb-[30px]">
-        {data.map((item) => (
-          <ToDo key={`${title}-${item.id}`} data={item} />
+        {todoData?.map((item) => (
+          <ToDo
+            key={`todo-${item.id}`}
+            data={item}
+            setIsChanged={setIsChanged}
+          />
         ))}
       </ul>
     </li>
