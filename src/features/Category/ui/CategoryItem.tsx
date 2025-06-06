d13c7713b28c4fc8ae42f132896053bbd07abe04
saiani1 <saiani@naver.com>
2025-06-06 21:03:21 +0900

    category관련 api연결, 로직 수정

--- src/features/Category/ui/CategoryItem.tsx
+++ src/features/Category/ui/CategoryItem.tsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 
 import type { CategoryType } from '@entities/Category';
-import { DoughnutChart } from '@shared/ui';
 
 type CategoryItemType = {
   data: CategoryType;
@@ -13,25 +12,14 @@ export const CategoryItem = ({ data }: CategoryItemType) => {
       <Link
         to={String(data.id)}
         className="flex flex-col justify-between p-[20px] w-[150px] h-[150px] bg-white shadow-md shadow-gray-200 rounded-lg"
+        state={data}
       >
-        <DoughnutChart
-          value={80}
-          maxValue={100}
-          outerStyle="flex justify-center items-center w-[56px] h-[56px]"
-          innerStyle="w-[40px] h-[40px]"
-          backgroundColor="bg-gray-100"
-          chartColor="#60a5fa"
-        >
-          <div className="w-[40px] h-[40px] rounded-full overflow-hidden">
-            <img src={data.category_image_url} alt={data.name} />
-          </div>
-        </DoughnutChart>
+        <div className="w-[50px] h-[50px] rounded-full border-2 border-white shadow-md overflow-hidden">
+          <img src={data.category_image_url} alt={data.name} />
+        </div>
         <div className="flex flex-col items-start">
           <span className="text-[18px] font-semibold text-gray-600">
             {data.name}
-          </span>
-          <span className="mt-[-3px] text-[12px] text-gray-400/80">
-            10/100 Tasks
           </span>
         </div>
       </Link>
--- src/pages/Category/ui/CategoryListScreen.tsx
+++ src/pages/Category/ui/CategoryListScreen.tsx
@@ -1,14 +1,53 @@
+import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { toast } from 'react-hot-toast';
 
 import { CategoryList } from '@features/Category';
-import { DUMMY_CATEGORY } from '@entities/Category';
+import type { CategoryType } from '@entities/Category';
 import { UserDataAtom } from '@entities/User';
 import { CommonButton, DoughnutChart, PlusLinkButton } from '@shared/ui';
 import { supabase } from '@shared/api';
+import type { ToDoType } from '@entities/ToDoList';
 
 const CategoryListScreen = () => {
   const [userData, setUserData] = useRecoilState(UserDataAtom);
+  const [categoryData, setCategoryData] = useState<CategoryType[]>();
+  const [allToDoLen, setAllToDoLen] = useState({
+    total: 0,
+    checked: 0,
+  });
+
+  const countData = (data: ToDoType[]) => {
+    return data.reduce(
+      (acc, todo) => {
+        if (todo.is_checked) {
+          acc.checked += 1;
+        }
+        return acc;
+      },
+      { checked: 0 }
+    );
+  };
+
+  useEffect(() => {
+    (async () => {
+      const { data, error } = await supabase.from('categories').select('*');
+      if (error) console.log('카테고리 리스트 가져오기 실패');
+      if (data) setCategoryData(data);
+
+      const { data: todo, error: todoError } = await supabase
+        .from('todos')
+        .select('*');
+      if (todoError) console.log('카테고리 리스트 가져오기 실패');
+      if (todo) {
+        const todoCnt = countData(todo);
+        setAllToDoLen({
+          ...todoCnt,
+          total: todo.length,
+        });
+      }
+    })();
+  }, []);
 
   const handleClickLogOut = async () => {
     let { error } = await supabase.auth.signOut();
@@ -21,18 +60,20 @@ const CategoryListScreen = () => {
     <div className="relative flex flex-col w-full h-screen px-[20px] py-[30px] bg-gray-50 overflow-auto">
       <div className="flex flex-col justify-center items-center gap-y-3 my-[30px]">
         <DoughnutChart
-          value={20}
-          maxValue={100}
+          value={allToDoLen.checked}
+          maxValue={allToDoLen.total}
           outerStyle="flex justify-center items-center w-[150px] h-[150px]"
           innerStyle="w-[130px] h-[130px]"
           backgroundColor="bg-gray-200"
           chartColor="#60a5fa"
         >
-          <div className="w-[130px] h-[130px] rounded-full overflow-hidden">
+          <div className="w-[120px] h-[120px] rounded-full overflow-hidden">
             <img src={userData?.profile_image_url} alt="유저 프로필 이미지" />
           </div>
         </DoughnutChart>
-        <span className="text-[13px] text-gray-400">20/100 tasks</span>
+        <span className="text-[13px] text-gray-400">
+          {allToDoLen.checked}/{allToDoLen.total} tasks
+        </span>
         <h1 className="-mt-[10px] text-[20px] text-gray-600 font-semibold">
           {userData?.nickname}
         </h1>
@@ -47,7 +88,7 @@ const CategoryListScreen = () => {
       <h1 className="my-[20px] ml-[2px] font-semibold text-[30px] text-gray-600">
         Lists
       </h1>
-      <CategoryList data={DUMMY_CATEGORY} />
+      {categoryData && <CategoryList data={categoryData} />}
       <PlusLinkButton href="createCategory" />
     </div>
   );
