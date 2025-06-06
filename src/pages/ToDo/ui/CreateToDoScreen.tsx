2357eb9a20eda63b99d048975a959db0c1a21093
saiani1 <saiani@naver.com>
2025-06-06 10:35:32 +0900

    createToDoScreen컴포넌트에 todo수정로직 추가

--- src/pages/ToDo/ui/CreateToDoScreen.tsx
+++ src/pages/ToDo/ui/CreateToDoScreen.tsx
@@ -1,20 +1,71 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import { IoCloseOutline } from 'react-icons/io5';
-import { FaRegBell } from 'react-icons/fa';
+import { FaRegBell, FaRegStar } from 'react-icons/fa';
 import { TbTag } from 'react-icons/tb';
 
-import { CategorySelectBox } from '@features/Category';
+import { CategorySelectBox, type CategorySelectType } from '@features/Category';
+import { CreateLi } from '@features/CreateTodo';
 import { DUMMY_CATEGORY } from '@entities/Category';
+import { convertToDatetimeLocal, type ToDoType } from '@entities/ToDoList';
 import { CommonButton, CommonInput } from '@shared/ui';
-import { Link } from 'react-router-dom';
 
 const CreateToDoScreen = () => {
-  const [clickedCategory, setClickedCategory] = useState('카테고리');
+  const { state } = useLocation();
+  const [clickedCategory, setClickedCategory] = useState<CategorySelectType>({
+    id: 0,
+    name: '카테고리',
+  });
+  const {
+    watch,
+    setValue,
+    reset,
+    register,
+    handleSubmit,
+    formState: { isDirty, errors },
+  } = useForm<ToDoType>({ mode: 'onChange' });
+
+  useEffect(() => {
+    if (state) {
+      const transformDate = convertToDatetimeLocal(state.date);
+      const transformToDo = {
+        ...state,
+        date: transformDate,
+      };
+      reset(transformToDo);
+      const filterCategory = DUMMY_CATEGORY.find(
+        (category) => category.id === state.category_id
+      );
+      setClickedCategory({
+        id: state.category_id,
+        name: filterCategory!.name,
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    setValue('category_id', clickedCategory.id, { shouldDirty: true });
+  }, [clickedCategory]);
+
+  const handleClickButton = () => {
+    const value = watch('is_important');
+    setValue('is_important', !value);
+  };
+
+  const submit = (data: ToDoType) => {
+    console.log('data', data);
+  };
 
   return (
-    <form className="flex flex-col justify-between w-full h-screen">
+    <form
+      className="relative flex flex-col w-full h-screen"
+      onSubmit={handleSubmit(submit)}
+    >
       <div className="relative flex justify-center items-center pt-[30px]">
-        <h1 className="text-gray-800 text-[20px]">새 일정 만들기</h1>
+        <h1 className="text-gray-800 text-[20px]">
+          {state ? '일정 수정하기' : '새 일정 만들기'}
+        </h1>
         <Link to="/" className="absolute right-[10px]">
           <IoCloseOutline size="30" stroke="#555" />
         </Link>
@@ -32,36 +83,50 @@ const CreateToDoScreen = () => {
           <CommonInput
             id="content"
             className="mt-[10px] py-2 w-full border-b border-gray-300"
+            {...register('content', { required: true })}
           />
         </div>
         <ul className="flex flex-col gap-y-[15px] ">
-          <li className="flex items-center gap-x-3 text-gray-400 focus-within:text-blue-400">
-            <label htmlFor="alert" className="transition-colors duration-300">
-              <FaRegBell size="20" className="text-inherit" />
-            </label>
+          <CreateLi
+            liStyle="gap-x-3"
+            labelFor="alert"
+            Icon={FaRegBell}
+            iconSize="20"
+          >
             <CommonInput
               type="datetime-local"
               id="alert"
               className="px-3 py-1 rounded-2xl text-gray-600"
+              {...register('date', { required: true })}
             />
-          </li>
-          <li className="flex items-center gap-x-3 font-medium text-gray-400 focus-within:text-blue-400">
-            <label className="transition-colors duration-300">
-              <TbTag size="22" className="text-inherit" />
-            </label>
+          </CreateLi>
+          <CreateLi liStyle="gap-x-3 font-medium" Icon={TbTag} iconSize="22">
             <CategorySelectBox
               data={DUMMY_CATEGORY}
               clickedOption={clickedCategory}
               setClickedOption={setClickedCategory}
             />
-          </li>
+          </CreateLi>
+          <CreateLi
+            liStyle="gap-x-6 text-gray-400"
+            Icon={FaRegStar}
+            iconSize="22"
+          >
+            <CommonButton
+              className={`px-[10px] py-[3px] text-white font-medium text-[14px] rounded-md ${watch('is_important') ? 'bg-amber-400' : 'bg-gray-400'} `}
+              onClick={handleClickButton}
+            >
+              IMPORTANT
+            </CommonButton>
+          </CreateLi>
         </ul>
       </div>
       <CommonButton
         type="submit"
-        className="py-[10px] w-full text-white text-[18px] font-medium bg-blue-400"
+        disabled={Object.keys(errors).length !== 0 || !isDirty}
+        className={`absolute bottom-0 py-[10px] w-full text-white text-[18px] font-medium ${Object.keys(errors).length || !isDirty ? 'bg-gray-400' : 'bg-blue-400'}`}
       >
-        생성
+        {state ? '수정' : '생성'}
       </CommonButton>
     </form>
   );
